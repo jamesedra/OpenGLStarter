2ddb87eb7b50b38505b601793236d31800ddea36
jamesedra <delstherjamesedralin@gmail.com>
2025-02-09 20:44:30 -0800

    unlit base color setup

--- OpenGLStarter/src/shaders/shader.h
+++ OpenGLStarter/src/shaders/shader.h
@@ -7,6 +7,11 @@
 #include <sstream>
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
 class Shader
 {
 public:
@@ -23,4 +28,6 @@ public:
 	void setBool(const std::string& name, bool value) const;
 	void setInt(const std::string& name, int value) const;
 	void setFloat(const std::string& name, float value) const;
+	void setVec3(const std::string& name, float x, float y, float z) const;
+	void setMat4(const std::string& name, const glm::mat4& mat) const;
 };
